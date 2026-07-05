79ce01032cab07917b0d45f4e89398e506d46bcd
Deepakraj S <deepakraj.s@canopi.in>
2026-07-05 16:38:39 +0530

    Initial Commit

---
+++ src/main/java/com/deepakraj/unique_id_generator/UniqueIdGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.deepakraj.unique_id_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueIdGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueIdGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepakraj/unique_id_generator/UniqueIdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepakraj.unique_id_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueIdGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
